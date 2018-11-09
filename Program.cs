dc3d24792c3a1c03ca78268878c13044e909c610
Vasyl Poklitar <poklitar@glooni.com>
2018-11-09 17:57:43 +0200

    Show example

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace ParseSchedule
         static void Main(string[] args)
         {
             Console.WriteLine("Set connetion");
-            FileInfo existingFile = new FileInfo("d:\\schedule1.xlsx");
+            FileInfo existingFile = new FileInfo("c:\\schedule1.xlsx");
             Console.OutputEncoding = Encoding.UTF8;
 
             using (Package = new ExcelPackage(existingFile))
@@ -28,6 +28,17 @@ namespace ParseSchedule
 
             Console.WriteLine("Specialities count: {0}/39", Schedule.Specialities.Count);
             Console.WriteLine("Groups count: {0}/50", Schedule.Groups.Count);
+            Console.WriteLine("-------------------------------------------");
+
+            foreach (var item in Schedule.Lessons.First(c => c.GroupName == "108").Lessons)
+            {
+                Console.WriteLine("Subject: {0}", item.Lesson.Name);
+                Console.WriteLine("Teacher: {0}", item.Teacher?.Name);
+                Console.WriteLine("Auditory: {0}", item.Auditory?.Name);
+                Console.WriteLine("-------------------------------------------");
+
+            }
+
             Console.ReadLine();
         }
     }
